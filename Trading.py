48d46ac93c67fef76714d67338908d562372316f
Belayet Hossain <belayet.bel.22@gmail.com>
2023-10-18 18:03:06 +0600

    Trading.py
    abc

---
+++ Trading.py
@@ -0,0 +1,785 @@
+""" f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+f = open("demofile2.txt", "r")
+print(f.read())
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+f = open("demofile2.txt", "r")
+print(f.read())
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
+quantity = 3
+itemno = 567
+price = 49
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+"""
+import random
+import time
+
+while True:
+    curancy = input("Enter The curancy: ")
+
+    num = random.randint(1, 3)
+    p = random.randint(55, 70)
+
+    time.sleep(num)
+
+    if(num == 1):
+        print("UP")
+        print(f"Aqurancy is: {p} %")
+
+    elif(num == 2):
+        print("STRATE; not recomendet to ")
+
+    elif(num == 3):
+        print("DOWN")
+        print(f"Aqurancy is: {p} %")
+    
+    time.sleep(20)
+"""f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+f = open("demofile2.txt", "r")
+print(f.read())
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+f = open("demofile2.txt", "r")
+print(f.read())
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve
+
+n = 10000
+ratio = .95
+n_0 = int((1-ratio) * n)
+n_1 = int(ratio * n)
+
+y = np.array([0] * n_0 + [1] * n_1)
+# below are the probabilities obtained from a hypothetical model that always predicts the majority class
+# probability of predicting class 1 is going to be 100%
+y_proba = np.array([1]*n)
+y_pred = y_proba > .5
+
+print(f'accuracy score: {accuracy_score(y, y_pred)}')
+cf_mat = confusion_matrix(y, y_pred)
+print('Confusion matrix')
+print(cf_mat)
+print(f'class 0 accuracy: {cf_mat[0][0]/n_0}')
+print(f'class 1 accuracy: {cf_mat[1][1]/n_1}')
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve
+
+n = 10000
+ratio = .95
+n_0 = int((1-ratio) * n)
+n_1 = int(ratio * n)
+
+y = np.array([0] * n_0 + [1] * n_1)
+# below are the probabilities obtained from a hypothetical model that always predicts the majority class
+# probability of predicting class 1 is going to be 100%
+y_proba = np.array([1]*n)
+y_pred = y_proba > .5
+
+print(f'accuracy score: {accuracy_score(y, y_pred)}')
+cf_mat = confusion_matrix(y, y_pred)
+print('Confusion matrix')
+print(cf_mat)
+print(f'class 0 accuracy: {cf_mat[0][0]/n_0}')
+print(f'class 1 accuracy: {cf_mat[1][1]/n_1}')
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve
+
+n = 10000
+ratio = .95
+n_0 = int((1-ratio) * n)
+n_1 = int(ratio * n)
+
+y = np.array([0] * n_0 + [1] * n_1)
+# below are the probabilities obtained from a hypothetical model that always predicts the majority class
+# probability of predicting class 1 is going to be 100%
+y_proba = np.array([1]*n)
+y_pred = y_proba > .5
+
+print(f'accuracy score: {accuracy_score(y, y_pred)}')
+cf_mat = confusion_matrix(y, y_pred)
+print('Confusion matrix')
+print(cf_mat)
+print(f'class 0 accuracy: {cf_mat[0][0]/n_0}')
+print(f'class 1 accuracy: {cf_mat[1][1]/n_1}')
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+k_folds = KFold(n_splits = 5)
+
+scores = cross_val_score(clf, X, y, cv = k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+k_folds = KFold(n_splits = 5)
+
+scores = cross_val_score(clf, X, y, cv = k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+k_folds = KFold(n_splits = 5)
+
+scores = cross_val_score(clf, X, y, cv = k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+k_folds = KFold(n_splits = 5)
+
+scores = cross_val_score(clf, X, y, cv = k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+print(logit.fit(X,y))
+
+print(logit.score(X,y))
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+print(logit.fit(X,y))
+
+print(logit.score(X,y))
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+print(logit.fit(X,y))
+
+print(logit.score(X,y))
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+print(logit.fit(X,y))
+
+print(logit.score(X,y))
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+print(logit.fit(X,y))
+
+print(logit.score(X,y))
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+
+scores = []
+
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+  import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+  import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+  import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+  import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+  import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+  import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+  import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)"""
